199ac62d921deb7aee4bdcff256f0d4e6ab5af27
Nenad Novakovic <dvlden@users.noreply.github.com>
2024-01-29 22:44:31 +0100

    fix: use tailwind import instead of plain css

--- examples/vanilla-tailwindcss/entrypoints/popup/main.ts
+++ examples/vanilla-tailwindcss/entrypoints/popup/main.ts
@@ -1,21 +1,27 @@
-import './style.css';
+import '@/assets/main.css';
 import typescriptLogo from '@/assets/typescript.svg';
 import viteLogo from '/wxt.svg';
 import { setupCounter } from '@/components/counter';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://wxt.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="WXT logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>WXT + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="flex flex-col items-center justify-center min-w-[320px] min-h-screen text-center bg-neutral-50 text-neutral-800 dark:text-neutral-100 dark:bg-neutral-800 antialiased p-8">
+    <div class="flex gap-12 mb-8">
+      <a href="https://wxt.dev" target="_blank" class="font-medium text-neutral-600 hover:text-neutral-500 no-underline">
+        <img src="${viteLogo}" class="h-12 hover:[filter:drop-shadow(0_0_2rem_#54bc4ae0)] transition-[filter]" alt="WXT logo" />
+      </a>
+
+      <a href="https://www.typescriptlang.org/" target="_blank" class="font-medium text-neutral-600 hover:text-neutral-500 no-underline">
+        <img src="${typescriptLogo}" class="h-12 hover:[filter:drop-shadow(0_0_2rem_#3178c6aa)] transition-[filter]" alt="TypeScript logo" />
+      </a>
     </div>
-    <p class="read-the-docs">
+
+    <h1 class="text-3xl font-semibold">WXT + TypeScript</h1>
+
+    <div class="p-8">
+      <button id="counter" type="button" class="rounded-lg border border-transparent px-3 py-1.5 font-medium bg-neutral-200 dark:bg-neutral-900 cursor-pointer hover:border-neutral-600 transition"></button>
+    </div>
+
+    <p class="text-neutral-600 dark:text-neutral-400">
       Click on the WXT and TypeScript logos to learn more
     </p>
   </div>
--- examples/vanilla-tailwindcss/tailwind.config.js
+++ examples/vanilla-tailwindcss/tailwind.config.js
@@ -1,7 +1,13 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./entrypoints/**/*.{html,ts}', './components/**/*.ts'],
+  darkMode: 'media',
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
